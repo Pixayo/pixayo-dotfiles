88295a63a2bde74c8ff5b31e4a4da8bc4587334e
Pixayo <kaio.rodrigo729@gmail.com>
2025-12-26 13:23:53 -0300

    checkpoint

--- system/configuration.nix
+++ system/configuration.nix
@@ -1,6 +1,3 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
 {
   config,
   pkgs,
@@ -8,14 +5,8 @@
   ...
 }: {
   imports = [
-    # Include the results of the hardware scan.
     ./hardware-configuration.nix
-    inputs.home-manager.nixosModules.default
-    # ./main-user.nix
   ];
-
-  # main-user.enable = true;
-  # main-user.userName = "kaio";
 
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
@@ -82,11 +73,6 @@
     extraGroups = ["networkmanager" "wheel"];
 
     shell = pkgs.zsh;
-  };
-
-  home-manager = {
-    extraSpecialArgs = {inherit inputs;};
-    users.kaio = import ./home.nix;
   };
 
   environment.systemPackages = with pkgs; [
