867f24d6ca603d6964ac69b808a49db62a7f8393
Pixayo <kaio.rodrigo729@gmail.com>
2025-12-30 10:53:45 -0300

    Home Manager standalone setup

--- home-manager/myPrograms/zsh.nix
+++ home-manager/myPrograms/zsh.nix
@@ -32,10 +32,24 @@ in {
         gs = "git status";
         gsave = "git add -A && git commit -m 'Checkpoint'";
 
-        # NixOS
+        # NixOS management
         cdc = "cd /etc/nixos";
         update = "sudo nix flake update";
-        rebuild = "sudo nixos-rebuild switch --flake .#nixos";
+        rebuild = "sudo nixos-rebuild switch --flake /etc/nixos#nixos";
+
+        # Home Manager
+        hm = "home-manager";
+        cdhm = "cd ~/.config/home-manager";
+
+        hmr = "home-manager switch --flake ~/.config/home-manager#kaio";
+        hmup = "cd ~/.config/home-manager && nix flake update";
+
+        hmtest = "home-manager build  --flake ~/.config/home-manager#kaio";
+        hmrollback = "home-manager switch --rollback";
+
+        hmclean = "nix-collect-garbage --delete-older-than 7d";
+        hmshow = "nix flake show ~/.config/home-manager";
+        hmgen = "home-manager generations";
       };
     };
   };
