8882b7ceec6ec1567d5817dfc427a3e1c4fc5b77
Pixayo <kaio.rodrigo729@gmail.com>
2026-01-02 10:40:18 -0300

    Add aliases data

---
+++ home-manager/data/aliases.nix
@@ -0,0 +1,33 @@
+let
+  env = import ./env.nix;
+in {
+  # Git
+  gl = "git log";
+  gs = "git status";
+  gsave = "git add -A && git commit -m 'Checkpoint'";
+
+  # Home Manager
+  hm = "home-manager";
+  cdhm = "cd ${env.paths.home}";
+
+  hmr = "home-manager switch --flake ${env.paths.home}#${env.user.name}"; # clobbered erro: "-b backup"
+  hmupdate = "cd ${env.paths.home} && nix flake update";
+
+  hmtest = "home-manager build --flake ${env.paths.home}#${env.user.name}";
+  hmrollback = "home-manager switch --rollback";
+
+  hmclean = "nix-collect-garbage --delete-old";
+  hmshow = "nix flake show ${env.paths.home}";
+  hmgen = "home-manager generations";
+
+  # NixOS management
+  cdc = "cd ${env.paths.system}";
+  update = "sudo nix flake update";
+  rebuild = "sudo nixos-rebuild switch --flake ${env.paths.system}#nixos";
+
+  # Other commands:
+  # nixos-rebuild list-generations           # List system generations
+  # nix-collect-garbage --delete-old         # Clean user generations
+  # sudo nix-collect-garbage --delete-old    # Delete system-wide garbage
+  # sudo nix flake update                    # Update flake inputs
+}
