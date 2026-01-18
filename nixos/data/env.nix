b2601afae8d5d66cfe074a090c4153d84abb94ca
Pixayo <kaio.rodrigo729@gmail.com>
2026-01-18 17:18:54 -0300

    Update COSMIC DE module

--- nixos/data/env.nix
+++ nixos/data/env.nix
@@ -5,7 +5,7 @@
   system = "x86_64-linux"; # Change to match your system architecture
 
   # NOTE: "desktop" is formatted to lowercase before import.
-  desktop = "GNOME"; # Which DE to use. Defined in "./nixos/modules/desktop"
+  desktop = "COSMIC"; # Which DE to use. Defined in "./nixos/modules/desktop"
 
   # NOTE: Check users declaration in configuration.nix
 
--- nixos/modules/desktop/cosmic.nix
+++ nixos/modules/desktop/cosmic.nix
@@ -4,7 +4,10 @@
   # COSMIC environment.
 
   # Enable the COSMIC desktop environment
-  services.desktopManager.cosmic.enable = true;
+  services.desktopManager.cosmic = {
+    enable = true;
+    # xwayland = false; # You can disable Xwayland support for the COSMIC compositor. 
+  };
 
   # Enable Greetd/TuiGreet login manager.
   # See: https://github.com/NixOS/nixpkgs/blob/nixos-25.11/nixos/modules/services/display-managers/greetd.nix
@@ -27,8 +30,22 @@
   # Exclude some default applications.
   environment.cosmic.excludePackages = with pkgs; [
     cosmic-edit
-    cosmic-files
     cosmic-store
     cosmic-term
   ];
+  # WARNING from nixpkgs: 
+  # The `environment.cosmic.excludePackages` option was used to exclude some
+  # packages from the environment which also includes some packages that the
+  # maintainers of the COSMIC DE deem necessary for the COSMIC DE to start
+  # and initialize. 
+  #
+  # Excluding said packages creates a high probability that
+  # the COSMIC DE will fail to initialize properly, or completely. This is an
+  # unsupported use case. If this was not intentional, please assign an empty
+  # list to the `environment.cosmic.excludePackages` option. 
+  
+  # If you want to exclude non-essential packages, please look at the NixOS module 
+  # for the COSMIC DE and look for the essential packages in the `corePkgs` list.
+  # See: https://github.com/NixOS/nixpkgs/blob/master/nixos/modules/services/desktop-managers/cosmic.nix
+
 }
