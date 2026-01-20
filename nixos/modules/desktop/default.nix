053516d6e329da9b3f495f5aa6a15af8e626ac00
Pixayo <kaio.rodrigo729@gmail.com>
2026-01-20 18:28:21 -0300

    Update desktop default.nix

--- nixos/modules/desktop/default.nix
+++ nixos/modules/desktop/default.nix
@@ -4,17 +4,21 @@
   ...
 }: let
   # NOTE:
-  # The attribute "desktop" declared in "data.env.desktop" must be all lowercase
+  # The attribute "desktop" declared in "data.users.<current>.desktop" must be all lowercase
   # to match the config files in this directory.
   #
   # I tried to make a "normalize system" to the metadata directory (aka data), but
   # it got really complicated, so I remove it.
+
+  # This is a special "default.nix".
+
+  user = data.users.current.name;
   desktop = data.users.current.desktop;
 
   configPath = ./. + "/${desktop}.nix";
   validPath = builtins.pathExists configPath;
 in {
   imports = 
-    assert (lib.assertMsg validPath "Invalid 'data.env.desktop' passed: '${desktop}'");
+    assert (lib.assertMsg validPath "Invalid 'data.users.${user}.desktop' attribute: '${desktop}'");
     [configPath];
 }
