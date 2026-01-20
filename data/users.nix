3b16f906c1ef5bd062693ff867b10c8108c70bd8
Pixayo <kaio.rodrigo729@gmail.com>
2026-01-20 18:30:33 -0300

    Change desktop attr message

--- data/users.nix
+++ data/users.nix
@@ -3,7 +3,8 @@ let
     name = "kaio";
     homeDirectory = "/home/${name}";
 
-    # NOTE: "desktop" is used to find a desktop config module defined in: "./nixos/modules/desktop"
+    # NOTE: "desktop" is used to find a desktop config module defined in: "./nixos/modules/desktop",
+    # and to find DE-related home configs, you can see more in: "./home-manager/home.nix"
     desktop = "gnome";
   };
   # alice = rec { ... };
